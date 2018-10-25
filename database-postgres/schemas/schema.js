750ab0391657f82ed05dc412c0a34128582d3faf
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2018-10-25 18:43:34 -0500

    Created knexfile.js & creating schema

---
+++ database-postgres/schemas/schema.js
@@ -0,0 +1,50 @@
+exports.up = knex => {
+    return knex.schema
+    .createTable('users', table => {
+        table.increments('id').primary();
+        table.string('avatarUrl');
+        table.string('username');
+      })
+    .createTable('listings', table => {
+        table.increments('id').primary();
+        table.point('latLong');
+        table
+          .integer('reviewCount')
+          .unsigned();
+        table.text('neighborhoodDescription');
+        table.text('gettingAroundDescription');
+    })
+    .createTable('reviews', table => {
+        table.increments('id').primary();
+        table
+          .integer('userId')
+          .unsigned()
+          .references('id')
+          .inTable('users')
+          .onDelete('SET NULL');
+        table
+          .integer('listingsId')
+          .unsigned()
+          .references('id')
+          .inTable('listings')
+          .onDelete('SET NULL');
+        table.text('reviewText');
+        table.text('responseText');
+        table.boolean('flagged')
+          .defaultTo(false);
+        table.date('createdAt');
+        table.integer('accuracyRating');
+        table.integer('communicationRating');
+        table.integer('cleanlinessRating');
+        table.integer('locationRating');
+        table.integer('check_In_Rating');
+        table.integer('valueRating');
+    });
+};
+
+exports.down = knex => {
+    return knex.schema
+      .dropTableIfExists('users')
+      .dropTableIfExists('listings')
+      .dropTableIfExists('reviews');
+}
