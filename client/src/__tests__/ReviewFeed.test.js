58e69fad245715a0793c611920c06e5e1ef4be5e
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2018-10-29 18:13:43 -0500

    Added review feed test and review feed

---
+++ client/src/__tests__/ReviewFeed.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-testing-library';
+import ReviewFeed from '../components/ReviewFeed';
+
+describe('Review Feed Component', () => {
+  it('renders a thing', () => {});
+});
---
+++ client/src/components/ReviewFeed.jsx
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+class ReviewFeed extends Component {}
+
+export default ReviewFeed;
